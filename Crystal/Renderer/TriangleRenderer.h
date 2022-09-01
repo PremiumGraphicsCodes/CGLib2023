50f45b772421504b6696aa2d23037688f3b6172c
mabuchi <mabuchi@premiumgraphics.co.jp>
2022-09-01 12:31:04 +0900

    add triangle shader.

---
+++ Crystal/Renderer/TriangleRenderer.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "IRenderer.h"
+#include "CGLib/Math/Matrix4d.h"
+#include "CGLib/Shader/VertexBufferObject.h"
+
+namespace Crystal {
+	namespace Renderer {
+
+class TriangleRenderer : public IRenderer
+{
+public:
+	struct Buffer
+	{
+		Shader::VertexBufferObject* position;
+		Shader::VertexBufferObject* color;
+		Math::Matrix4dd projectionMatrix;
+		Math::Matrix4dd modelViewMatrix;
+		std::vector<unsigned int> indices;
+	};
+
+	void link() override;
+
+	void render() override;
+
+	Buffer buffer;
+};
+
+	}
+}
