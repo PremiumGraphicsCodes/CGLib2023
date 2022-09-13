9854c1eae728307a6a35092a771b36f8319eb485
mabuchi <mabuchi@premiumgraphics.co.jp>
2022-09-13 13:54:28 +0900

    implement space lib.

---
+++ Crystal/Space/ZOrderCurve2d.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <array>
+
+namespace Crystal {
+	namespace Space {
+
+		class ZOrderCurve2d
+		{
+		public:
+			unsigned int encode(const std::array<unsigned int, 2>& index) const;
+
+			std::array<unsigned int, 2> decode(unsigned int x) const;
+
+		private:
+			unsigned int encode(unsigned int n) const;
+
+			unsigned int decode_(unsigned int x) const;
+		};
+
+	}
+}
