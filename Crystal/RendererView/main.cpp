5e415b4a582145f688b0cfdcadb249d1eefa489d
mabuchi <mabuchi@premiumgraphics.co.jp>
2022-09-15 11:00:24 +0900

    refactor renderer.

--- Crystal/RendererView/main.cpp
+++ Crystal/RendererView/main.cpp
@@ -1,11 +1,5 @@
-#include "../../CGLib/Shader/glew.h"
-
-#include "../../CGLib/UI/imgui.h"
-#include "../../CGLib/UI/imgui_impl_glfw.h"
-#include "../../CGLib/UI/imgui_impl_opengl3.h"
-#include "GLFW/glfw3.h"
-
 #include <stdio.h>
+#include <iostream>
 
 #include "PointShader.h"
 #include "LineShader.h"
@@ -17,11 +11,15 @@
 #include "DFPolygonShader.h"
 #include "IBLShader.h"
 
-//#include "Crystal/ThirdParty/glew-2.1.0/include/GL/glew.h"
-
-#include <iostream>
 
 #include "CGLib/Math/Vector2d.h"
+
+#include "Crystal/AppBase/IRenderer.h"
+#include "Crystal/AppBase/MenuItem.h"
+#include "Crystal/AppBase/CameraUICtrl.h"
+#include "Crystal/AppBase/Canvas.h"
+#include "Crystal/AppBase/Window.h"
+
 
 using namespace Crystal::Math;
 using namespace Crystal::Graphics;
@@ -29,228 +27,79 @@ using namespace Crystal::Renderer;
 
 namespace {
 
-	void glfw_error_callback(int error, const char* description)
+	class Renderer : public Crystal::UI::IRenderer
 	{
-		fprintf(stderr, "Glfw Error %d: %s\n", error, description);
-	}
+	public:
+		Renderer() :
+			camera(Vector3df(0, 0, 1), Vector3df(0, 0, 0), Vector3df(0, 1, 0), 0.1, 10.0)
+		{}
 
-	bool isLeftDown;
-	bool isRightDown;
+		void init() override
+		{
+			pointRenderer.build();
+			lineRenderer.build();
+			triangleRenderer.build();
+			texRenderer.build();
+			pbLightRenderer.build();
+			skyBoxRenderer.build();
+			dfRenderer.build();
+			iblRenderer.build();
 
-	Camera camera(Vector3df(0, 0, 1), Vector3df(0, 0, 0), Vector3df(0, 1, 0), 0.1, 10.0);
-
-	PointShader pointRenderer;
-	LineShader lineRenderer;
-	TriangleShader triangleRenderer;
-	TexShader texRenderer;
-
-	PBLightShader pbLightRenderer;
-	SkyBoxShader skyBoxRenderer;
-	DFPolygonShader dfRenderer;
-	IBLShader iblRenderer;
-	IScreenShader* activeRenderer = &pbLightRenderer;
-
-
-	Vector2df toScreenCoord(GLFWwindow* window, const double x, const double y) {
-		int width, height;
-		glfwGetWindowSize(window, &width, &height);
-		const auto xx = x / (float)width;
-		const auto yy = 1.0 - y / (float)height;
-		return Vector2df(xx, yy);
-	}
-
-	Vector2df prevCoord(0, 0);
-
-	void onMouse(GLFWwindow* window, int button, int action, int mods) {
-		if (ImGui::IsMouseHoveringAnyWindow()) {
-			return;
+			activeRenderer = &pointRenderer;
 		}
 
-		double x, y;
-		glfwGetCursorPos(window, &x, &y);
-		const auto& coord = toScreenCoord(window, x, y);
-		if (button == GLFW_MOUSE_BUTTON_LEFT) {
-			if (action == GLFW_PRESS) {
-				prevCoord = coord;
-				//canvas->onLeftButtonDown(coord);
-				isLeftDown = true;
-			}
-			else if (action == GLFW_RELEASE) {
-				//canvas->onLeftButtonUp(coord);
-				isLeftDown = false;
-			}
-		}
-		else if (button == GLFW_MOUSE_BUTTON_RIGHT) {
-			if (action == GLFW_PRESS) {
-				prevCoord = coord;
-				//canvas->onRightButtonDown(coord);
-				isRightDown = true;
-			}
-			else if (action == GLFW_RELEASE) {
-				//canvas->onRightButtonUp(coord);
-				isRightDown = false;
-			}
-		}
-	}
+		void render(const int width, const int height) override
+		{
+			glClearColor(0, 0, 0, 0);
+			glClear(GL_COLOR_BUFFER_BIT);
 
 
-	void onMouseMove(GLFWwindow* window, double xpos, double ypos)
+			activeRenderer->render(camera, width, height);
+
+		}
+
+		Camera camera;
+
+		PointShader pointRenderer;
+		LineShader lineRenderer;
+		TriangleShader triangleRenderer;
+		TexShader texRenderer;
+
+		PBLightShader pbLightRenderer;
+		SkyBoxShader skyBoxRenderer;
+		DFPolygonShader dfRenderer;
+		IBLShader iblRenderer;
+		IScreenShader* activeRenderer = &pbLightRenderer;
+
+	};
+
+	class RendererMenu : public Crystal::UI::IMenu
 	{
-		const auto& coord = toScreenCoord(window, xpos, ypos);
-		if (isLeftDown) {
-			const auto diff = (coord - prevCoord) * 1.0f;
-			camera.setEye(camera.getEye() + Vector3df(diff, 0.0));
-			camera.setTarget(camera.getTarget() + Vector3df(diff, 0.0));
-			prevCoord = coord;
-			//canvas->onLeftDragging(coord);
+	public:
+		explicit RendererMenu(const std::string& name, Renderer* renderer) :
+			IMenu(name)
+		{
+			add(new Crystal::UI::MenuItem("Point", [&renderer]() { renderer->activeRenderer = &renderer->pointRenderer; }));
+			add(new Crystal::UI::MenuItem("Line", [&renderer]() { renderer->activeRenderer = &renderer->lineRenderer; }));
+			add(new Crystal::UI::MenuItem("Triangle", [&renderer]() { renderer->activeRenderer = &renderer->triangleRenderer; }));
+			add(new Crystal::UI::MenuItem("Tex", [&renderer]() { renderer->activeRenderer = &renderer->texRenderer; }));
+			add(new Crystal::UI::MenuItem("SkyBox", [&renderer]() { renderer->activeRenderer = &renderer->skyBoxRenderer; }));
+			add(new Crystal::UI::MenuItem("PBLight", [&renderer]() { renderer->activeRenderer = &renderer->pbLightRenderer; }));
+			add(new Crystal::UI::MenuItem("DFPolygon", [&renderer]() { renderer->activeRenderer = &renderer->dfRenderer; }));
+			add(new Crystal::UI::MenuItem("IBL", [&renderer]() { renderer->activeRenderer = &renderer->iblRenderer; }));
 		}
-		else if (isRightDown) {
-			const auto diff = prevCoord - coord;
-			//const auto bb = world->getBoundingBox();
-			const auto scale = 1.0;//glm::distance(bb.getMin(), bb.getMax()) * 0.1;
-
-			const auto& matrix = camera.getRotationMatrix();
-			const auto v = glm::transpose(Matrix3dd(matrix)) * Vector3dd(diff, 0.0);
-
-			{
-				const Vector3df t = v;
-				const auto& eye = camera.getEye();
-				const auto& target = camera.getTarget();
-				const auto& up = camera.getUp();
-				camera.lookAt(eye + t, target, up);
-			}
-
-			prevCoord = coord;
-		}
-	}
+	};
 }
 
+
 int main() {
-    if (!glfwInit()) {
-        return false;
-    }
+	Renderer renderer;
+	auto uiCtrl = std::make_unique<Crystal::UI::CameraUICtrl>(&renderer.camera);
 
-    //glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    //glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    auto window = glfwCreateWindow(1280, 720, "Hello", NULL, NULL);
-    glfwMakeContextCurrent(window);
-    //	gl3wInit();
+	Crystal::UI::Canvas canvas(std::move(uiCtrl), &renderer);
+	Crystal::UI::Window app("Hello", &canvas);
+	app.init();
+	app.add(new RendererMenu("Renderer", &renderer));
 
-    const auto e = glewInit();
-    if (e != GLEW_OK) {
-        return false;
-    }
-
-
-    // Setup ImGui binding
-    //ImGui_ImplGlfwGL3_Init(window, true);
-
-    const char* glsl_version = "#version 130";
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;   // Enable Gamepad Controls
-
-    // Setup Dear ImGui style
-    ImGui::StyleColorsDark();
-    //ImGui::StyleColorsClassic();
-
-    // Setup Platform/Renderer bindings
-    ImGui_ImplGlfw_InitForOpenGL(window, true);
-    ImGui_ImplOpenGL3_Init(glsl_version);
-
-
-   // glfwSetScrollCallback(window, onWheel);
-   glfwSetMouseButtonCallback(window, onMouse);
-   glfwSetCursorPosCallback(window, onMouseMove);
-
-   // glfwSetWindowCloseCallback(window, onClose);
-
-
-    // Setup window
-    //glfwSetErrorCallback(glfw_error_callback);
-   if (!glfwInit()) {
-	   return 1;
-   }
-
-	pointRenderer.build();
-	lineRenderer.build();
-	triangleRenderer.build();
-	texRenderer.build();
-	pbLightRenderer.build();
-	skyBoxRenderer.build();
-	dfRenderer.build();
-	iblRenderer.build();
-
-   // onInit();
-
-	while (!glfwWindowShouldClose(window)) {
-		// Start the Dear ImGui frame
-		ImGui_ImplOpenGL3_NewFrame();
-		ImGui_ImplGlfw_NewFrame();
-		ImGui::NewFrame();
-
-		if (ImGui::BeginMainMenuBar()) {
-			if (ImGui::BeginMenu("Renderer")) {
-				if (ImGui::MenuItem("Point")) {
-					::activeRenderer = &pointRenderer;
-				}
-				if (ImGui::MenuItem("Line")) {
-					::activeRenderer = &lineRenderer;
-				}
-				if (ImGui::MenuItem("Triangle")) {
-					::activeRenderer = &triangleRenderer;
-				}
-				if (ImGui::MenuItem("Tex")) {
-					::activeRenderer = &texRenderer;
-				}
-				if (ImGui::MenuItem("SkyBox")) {
-					::activeRenderer = &skyBoxRenderer;
-				}
-				if (ImGui::MenuItem("PBLight")) {
-					::activeRenderer = &pbLightRenderer;
-				}
-				if (ImGui::MenuItem("DFPolygon")) {
-					::activeRenderer = &dfRenderer;
-				}
-				if (ImGui::MenuItem("IBL")) {
-					::activeRenderer = &iblRenderer;
-				}
-
-				ImGui::EndMenu();
-			}
-			ImGui::EndMainMenuBar();
-		}
-
-		glClearColor(0, 0, 0, 0);
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		int width, height;
-		glfwGetWindowSize(window, &width, &height);
-
-		activeRenderer->render(camera, width, height);
-		//skyBoxRenderer.render(camera, width, height);
-        //onRender(width, height);
-		//world->getRenderer()->render(*world->getCamera()->getCamera(), width, height);
-		//const auto animations = world->getAnimations();
-		//for (auto& a : animations) {
-		//	a->step();
-		//}
-
-		glFlush();
-
-		ImGui::Render();
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-    }
-
-	// Cleanup
-	ImGui_ImplOpenGL3_Shutdown();
-	ImGui_ImplGlfw_Shutdown();
-
-	ImGui::DestroyContext();
-	glfwTerminate();
+	app.show();
 }
