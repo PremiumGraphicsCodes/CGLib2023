e8c52422e058af64122353d6a9b23f731d8b8f86
mabuchi <mabuchi@premiumgraphics.co.jp>
2022-09-27 12:12:32 +0900

    add test.

---
+++ Crystal/SpaceTest/IndexedSortBasedSearcherTest.cpp
@@ -0,0 +1,16 @@
+#include "gtest/gtest.h"
+
+#include "Crystal/Space/IndexedSortBasedSeacher.h"
+
+using namespace Crystal::Math;
+using namespace Crystal::Space;
+
+TEST(IndexedSortBasedSearcherTest, TestSearch)
+{
+	IndexedSortBasedSearcher searcher(1.0f);
+	searcher.add(Vector3df(0, 0, 0));
+	searcher.add(Vector3df(0.5, 0, 0));
+	searcher.search();
+	const auto pairs = searcher.getPairs();
+	EXPECT_EQ(1, pairs.size());
+}
