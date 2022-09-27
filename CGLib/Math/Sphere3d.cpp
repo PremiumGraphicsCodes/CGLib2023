6597a62738f2ac5bcf5539ef04a66d90de1e3167
mabuchi <mabuchi@premiumgraphics.co.jp>
2022-09-27 12:40:19 +0900

    add sphere3d.

---
+++ CGLib/Math/Sphere3d.cpp
@@ -0,0 +1,54 @@
+#include "Sphere3d.h"
+#include "Box3d.h"
+
+//#include <numeric>
+#include "pi.h"
+
+using namespace Crystal::Math;
+
+template<typename T>
+Sphere3d<T>::Sphere3d() :
+	center(Vector3dd(0, 0, 0)),
+	radius(1.0)
+{}
+
+template<typename T>
+Sphere3d<T>::Sphere3d(const Vector3d<T>& center, const T radius) :
+	center(center),
+	radius(radius)
+{}
+
+template<typename T>
+Vector3d<T> Sphere3d<T>::getPosition(const T u, const T v) const
+{
+	return getPosition(u, v, 1.0);
+}
+
+template<typename T>
+Vector3d<T> Sphere3d<T>::getPosition(const T u, const T v, const T w) const
+{
+	const auto theta = u * PI;
+	const auto phi = 2.0 * v * PI;
+	const auto x = w * radius * std::sin(theta) * std::cos(phi);
+	const auto y = w * radius * std::sin(theta) * std::sin(phi);
+	const auto z = w * radius * std::cos(theta);
+	return center + Vector3d<T>(x, y, z);
+}
+
+template<typename T>
+Vector3d<T> Sphere3d<T>::getNormal(const T u, const T v) const
+{
+	const auto& pos = getPosition(u, v, 1.0);
+	return glm::normalize(pos - center);
+}
+
+template<typename T>
+Box3d<T> Sphere3d<T>::getBoundingBox() const
+{
+	const auto min = center - Vector3d<T>(radius, radius, radius);
+	const auto max = center + Vector3d<T>(radius, radius, radius);
+	return Box3d<T>(min, max);
+}
+
+template class Sphere3d<float>;
+template class Sphere3d<double>;
---
+++ CGLib/Math/Sphere3d.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "Vector3d.h"
+
+namespace Crystal {
+	namespace Math {
+
+template<typename T>
+class Box3d;
+
+template<typename T>
+class Sphere3d
+{
+public:
+	Sphere3d();
+
+	Sphere3d(const Vector3d<T>& center, const T radius);
+
+	Vector3d<T> getPosition(const T u, const T v) const;// override;
+
+	Vector3d<T> getPosition(const T u, const T v, const T w) const; //const override;
+
+	Vector3d<T> getNormal(const T u, const T v) const;// const override;
+
+	Vector3d<T> getCenter() const { return center; }
+
+	T getRadius() const { return radius; }
+
+	Box3d<T> getBoundingBox() const;
+
+private:
+	Vector3d<T> center;
+	T radius;
+};
+
+using Sphere3df = Sphere3d<float>;
+using Sphere3dd = Sphere3d<double>;
+
+	}
+}
