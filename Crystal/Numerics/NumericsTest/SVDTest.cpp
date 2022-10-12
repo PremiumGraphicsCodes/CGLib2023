6d4dba2e002501d663658917184ae0f9b52ad221
mabuchi <mabuchi@premiumgraphics.co.jp>
2022-10-12 12:55:02 +0900

    add test.

---
+++ Crystal/Numerics/NumericsTest/SVDTest.cpp
@@ -0,0 +1,41 @@
+#include "pch.h"
+
+#include "../Numerics/SVD.h"
+
+#include "CGLib/Math/pi.h"
+
+using namespace Crystal::Math;
+using namespace Crystal::Numerics;
+
+
+namespace {
+	constexpr auto tolerance = 1.0e-6;
+}
+
+TEST(SVDTest, TestSolve2x2)
+{
+	{
+		Matrix2dd m(1, 2, 2, 3);
+		SVD2d svd;
+		const auto actual = svd.calculate(m);
+		const auto values = actual.eigenValues;
+		EXPECT_NEAR(-0.23606804013252258, values[0], tolerance);
+		EXPECT_NEAR(4.2360687255859375, values[1], tolerance);
+	}
+	{
+		Matrix2dd m = rotationMatrix(0.5 * PI);
+		SVD2d svd;
+		const auto actual = svd.calculate(m);
+		const auto values = actual.eigenValues;
+		EXPECT_NEAR(-1.0, values[0], tolerance);
+		EXPECT_NEAR(1.0, values[1], tolerance);
+	}
+	{
+		Matrix2dd m = rotationMatrix(PI);
+		SVD2d svd;
+		const auto actual = svd.calculate(m);
+		const auto values = actual.eigenValues;
+		EXPECT_NEAR(-1.0, values[0], tolerance);
+		EXPECT_NEAR(-1.0, values[1], tolerance);
+	}
+}
