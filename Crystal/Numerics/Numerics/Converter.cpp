cef2e31501972b26efc6178da8bc307839d1a004
mabuchi <mabuchi@premiumgraphics.co.jp>
2022-10-12 11:51:34 +0900

    add converter.

---
+++ Crystal/Numerics/Numerics/Converter.cpp
@@ -0,0 +1,102 @@
+#include "pch.h"
+
+#include "Converter.h"
+
+using namespace Crystal::Math;
+using namespace Crystal::Numerics;
+
+Eigen::Matrix2d Converter::toEigen(const Matrix2dd& src)
+{
+	Eigen::Matrix2d dest;
+	for (int i = 0; i < 2; ++i) {
+		for (int j = 0; j < 2; ++j) {
+			dest(i, j) = src[i][j];
+		}
+	}
+	return dest;
+}
+
+Eigen::Matrix3d Converter::toEigen(const Matrix3dd& src)
+{
+	Eigen::Matrix3d dest;
+	for (int i = 0; i < 3; ++i) {
+		for (int j = 0; j < 3; ++j) {
+			dest(i, j) = src[i][j];
+		}
+	}
+	return dest;
+}
+
+Eigen::Matrix4d Converter::toEigen(const Matrix4dd& src)
+{
+	Eigen::Matrix4d dest;
+	for (int i = 0; i < 4; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			dest(i, j) = src[i][j];
+		}
+	}
+	return dest;
+}
+
+Eigen::Vector2d Converter::toEigen(const Vector2dd& src)
+{
+	return Eigen::Vector2d(src[0], src[1]);
+}
+
+Eigen::Vector3d Converter::toEigen(const Vector3dd& src)
+{
+	return Eigen::Vector3d(src[0], src[1], src[2]);
+}
+
+Eigen::Vector4d Converter::toEigen(const Vector4dd& src)
+{
+	return Eigen::Vector4d(src[0], src[1], src[2], src[3]);
+}
+
+Matrix2dd Converter::fromEigen(const Eigen::Matrix2d& src)
+{
+	Matrix2dd dest;
+	for (int i = 0; i < 2; ++i) {
+		for (int j = 0; j < 2; ++j) {
+			dest[i][j] = src(i, j);
+		}
+	}
+	return dest;
+}
+
+Matrix3dd Converter::fromEigen(const Eigen::Matrix3d& src)
+{
+	Matrix3dd dest;
+	for (int i = 0; i < 3; ++i) {
+		for (int j = 0; j < 3; ++j) {
+			dest[i][j] = src(i, j);
+		}
+	}
+	return dest;
+}
+
+Matrix4dd Converter::fromEigen(const Eigen::Matrix4d& src)
+{
+	Matrix4dd dest;
+	for (int i = 0; i < 4; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			dest[i][j] = src(i, j);
+		}
+	}
+	return dest;
+}
+
+Vector2dd Converter::fromEigen(const Eigen::Vector2d& src)
+{
+	return Vector2dd(src[0], src[1]);
+}
+
+Vector3dd Converter::fromEigen(const Eigen::Vector3d& src)
+{
+	return Vector3dd(src[0], src[1], src[2]);
+}
+
+Vector4dd Converter::fromEigen(const Eigen::Vector4d& src)
+{
+	return Vector4dd(src[0], src[1], src[2], src[3]);
+}
---
+++ Crystal/Numerics/Numerics/Converter.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "CGLib/Math/Matrix2d.h"
+#include "CGLib/Math/Matrix3d.h"
+#include "CGLib/Math/Matrix4d.h"
+
+#include "CGLib/Math/Vector2d.h"
+#include "CGLib/Math/Vector3d.h"
+#include "CGLib/Math/Vector4d.h"
+
+#include "../ThirdParty/eigen-3.4.0/Eigen/Core"
+
+namespace Crystal {
+	namespace Numerics {
+
+class Converter
+{
+public:
+	static Eigen::Matrix2d toEigen(const Math::Matrix2dd& src);
+
+	static Eigen::Matrix3d toEigen(const Math::Matrix3dd& src);
+
+	static Eigen::Matrix4d toEigen(const Math::Matrix4dd& src);
+
+	static Eigen::Vector2d toEigen(const Math::Vector2dd& src);
+
+	static Eigen::Vector3d toEigen(const Math::Vector3dd& src);
+
+	static Eigen::Vector4d toEigen(const Math::Vector4dd& src);
+
+	static Math::Matrix2dd fromEigen(const Eigen::Matrix2d& src);
+
+	static Math::Matrix3dd fromEigen(const Eigen::Matrix3d& src);
+
+	static Math::Matrix4dd fromEigen(const Eigen::Matrix4d& src);
+
+	static Math::Vector2dd fromEigen(const Eigen::Vector2d& src);
+
+	static Math::Vector3dd fromEigen(const Eigen::Vector3d& src);
+
+	static Math::Vector4dd fromEigen(const Eigen::Vector4d& src);
+};
+	}
+}
