dddfe368c4ca6fc1888de03a5466f818a763a9fc
mabuchi <mabuchi@premiumgraphics.co.jp>
2022-10-14 12:11:39 +0900

    add MVPFluid.

---
+++ Crystal/Physics/MVPFluid.cpp
@@ -0,0 +1,90 @@
+#include "MVPFluid.h"
+
+#include <algorithm>
+#include <iterator>
+
+
+using namespace Crystal::Math;
+using namespace Crystal::Physics;
+
+MVPFluid::MVPFluid() :
+	pressureCoe(10000.0),
+	viscosityCoe(50.0)
+{
+}
+
+MVPFluid::~MVPFluid()
+{
+}
+
+/*
+Box3df MVPFluid::getBoundingBox() const
+{
+	Box3df bb = Box3df::createDegeneratedBox();
+	for (auto p : particles) {
+		bb.add(p->getPosition());
+	}
+	return bb;
+}
+
+void IMVPFluidScene::removeDegeneratedVolumes()
+{
+	this->particles.sort();
+	std::list<MVPVolumeParticle*> vps;
+	for (auto v : particles) {
+		if (v->getMassParticles().empty()) {
+			vps.push_back(v);
+		}
+	}
+	std::list<MVPVolumeParticle*> diffs;
+	std::set_difference(
+		this->particles.begin(), this->particles.end(),
+		vps.begin(), vps.end(),
+		std::inserter(diffs, diffs.end())
+	);
+	this->particles = diffs;
+	for (auto v : vps) {
+		delete v;
+	}
+	removeExpired();
+}
+
+void IMVPFluidScene::removeExpired()
+{
+	std::list<MVPVolumeParticle*> vps;
+	if (lifeLimit != -1) {
+		for (auto v : particles) {
+			if (v->getMassParticles().front()->lifeTime > lifeLimit) {
+				v->clearMasses();
+				vps.push_back(v);
+			}
+		}
+	}
+	std::list<MVPVolumeParticle*> diffs;
+	std::set_difference(
+		this->particles.begin(), this->particles.end(),
+		vps.begin(), vps.end(),
+		std::inserter(diffs, diffs.end())
+	);
+	this->particles = diffs;
+	for (auto v : vps) {
+		delete v;
+	}
+}
+
+MVPVolumeParticle* IMVPFluidScene::create(const Vector3df& position, const float radius, const float weight, const float temperature)
+{
+	auto vp = new MVPVolumeParticle(radius, position);
+	auto mp = new MVPMassParticle(vp, Vector3dd(0, 0, 0), weight);
+	mp->updateTemperature(1000.0f);
+	vp->setRestMass(weight * 1.25f);
+	vp->addMassParticle(mp);
+	vp->setTemperature(temperature);
+	mp->setParent(vp);
+
+	//vp->addMassParticle(mp);
+	return vp;
+	//	restMass *= 1.5;
+	//	selfMass = unum * vnum * wnum * weight;
+}
+*/
---
+++ Crystal/Physics/MVPFluid.h
@@ -0,0 +1,76 @@
+#pragma once
+
+#include <list>
+#include "CGLib/Math/Box3d.h"
+
+namespace Crystal {
+	namespace Physics {
+		class MVPVolumeParticle;
+
+class MVPFluid
+{
+public:
+	MVPFluid();
+
+	~MVPFluid();
+
+	void addParticle(MVPVolumeParticle* mp);
+	/*
+{
+	mp->setPressureCoe(this->pressureCoe);
+	mp->setViscosityCoe(this->viscosityCoe);
+	mp->setHeatDiffuseCue(this->heatDiffuseCoe);
+	mp->setDragHeatCoe(this->dragHeatCoe);
+	mp->setDragForceCoe(this->dragForceCoe);
+	particles.push_back(mp);
+}
+*/
+
+	std::list<MVPVolumeParticle*> getParticles() const { return particles; }
+
+	void remove(MVPVolumeParticle* p) { particles.remove(p); }
+
+	void removeDegeneratedVolumes();
+
+	void removeExpired();
+
+	void add(MVPVolumeParticle* p) { particles.push_back(p); }
+
+	Math::Box3df getBoundingBox() const;
+
+	void setPressureCoe(const float coe) { this->pressureCoe = coe; }
+
+	float getPressure() const { return this->pressureCoe; }
+
+	void setViscosityCoe(const float coe) { this->viscosityCoe = coe; }
+
+	float getViscosityCoe() const { return this->viscosityCoe; }
+
+	void setHeatDiffuseCoe(const float coe) { this->heatDiffuseCoe = coe; }
+
+	float getHeatDiffuseCoe() const { return this->heatDiffuseCoe; }
+
+	void setDragHeatCoe(const float c) { this->dragHeatCoe = c; }
+
+	float getDragHeatCoe() const { return this->dragHeatCoe; }
+
+	void setDragForceCoe(const float c) { this->dragForceCoe = c; }
+
+	float getDragForceCoe() const { return this->dragForceCoe; }
+
+	void setLifeLimit(const int limit) { this->lifeLimit = limit; }
+
+	MVPVolumeParticle* create(const Math::Vector3df& position, const float radius, const float weight, const float temperature);
+
+private:
+	std::list<MVPVolumeParticle*> particles;
+	float pressureCoe;
+	float viscosityCoe;
+	float heatDiffuseCoe;
+	float dragHeatCoe;
+	float dragForceCoe;
+	int lifeLimit = -1;
+};
+
+	}
+}
