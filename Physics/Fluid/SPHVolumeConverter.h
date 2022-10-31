220ef90de98937183d3985dba142a7ecf4c4eaa7
mabuchi <mabuchi@premiumgraphics.co.jp>
2022-10-31 11:52:59 +0900

    implement SPH surface.

--- Physics/Fluid/SPHVolumeConverter.h
+++ Physics/Fluid/SPHVolumeConverter.h
@@ -5,7 +5,7 @@
 
 #include "SPHSurfaceParticle.h"
 
-#include "Crystal/Space/SparseVolume.h"
+#include "Crystal/Volume/SparseVolume.h"
 
 namespace Crystal {
 	namespace Physics {
@@ -13,16 +13,16 @@ namespace Crystal {
 class SPHVolumeConverter
 {
 public:
-	std::unique_ptr<Space::SparseVolumef> buildIsotoropic(const std::vector<Math::Vector3dd>& positions, const float particleRadius, const float cellLength);
+	std::unique_ptr<Volume::SparseVolumef> buildIsotoropic(const std::vector<Math::Vector3dd>& positions, const float particleRadius, const float cellLength);
 
-	std::unique_ptr<Space::SparseVolumef> buildAnisotoropic(const std::vector<Math::Vector3dd>& positions, const float particleRadius, const float cellLength);
+	std::unique_ptr<Volume::SparseVolumef> buildAnisotoropic(const std::vector<Math::Vector3dd>& positions, const float particleRadius, const float cellLength);
 
 	const std::vector<std::unique_ptr<SPHSurfaceParticle>>& getParticles() const { return particles; }
 
 	//std::vector < Shape::Volume<double>* > getVolumes() { return volumes; }
 
 private:
-	std::unique_ptr<Space::SparseVolumef> createSparseVolume(const std::vector<Math::Vector3dd>& particles, const float cellLength);
+	std::unique_ptr<Volume::SparseVolumef> createSparseVolume(const std::vector<Math::Vector3dd>& particles, const float cellLength);
 
 	void calculateDensity(const float searchRadius);
 
