1ed5cf8874b5e7319d0eaa090f3dfab7f85259be
mabuchi <mabuchi@premiumgraphics.co.jp>
2022-11-11 09:43:52 +0900

    add PLYFile.

---
+++ SandBox/PC/PC/PLYFileReader.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <string>
+#include <filesystem>
+
+#include "CGLib/Math/Vector3d.h"
+#include "PLYFile.h"
+
+namespace Crystal {
+	namespace IO {
+
+class PLYFileReader
+{
+public:
+	bool read(const std::filesystem::path& filename);
+
+	bool read(std::istream& stream);
+
+	PLYFile getPLY() const { return ply; }
+
+private:
+	bool readAsciiData(std::istream& stream, const unsigned int count);
+
+	bool readBinaryData(std::istream& stream, const unsigned int count);
+
+private:
+	PLYFile ply;
+};
+
+	}
+}
---
+++ SandBox/PC/PC/STLFile.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "CGLib/Math/Triangle3d.h"
+
+namespace Crystal {
+	namespace IO {
+
+		struct STLFace
+		{
+			STLFace()
+			{};
+
+			explicit STLFace(const Math::Triangle3df& triangle) :
+				STLFace(triangle, triangle.getNormal())
+			{}
+
+			STLFace(const Math::Triangle3df& triangle, const Math::Vector3df& normal) :
+				triangle(triangle),
+				normal(normal)
+			{}
+
+			Math::Triangle3df triangle;
+			Math::Vector3df normal;
+		};
+
+		struct STLFile
+		{
+			std::string header;
+			size_t faceCount = 0;
+			std::vector<STLFace> faces;
+		};
+
+	}
+}
