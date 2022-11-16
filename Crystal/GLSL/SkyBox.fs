4f3aa47ccc4602804a9f89d3c2274a8e464f5dc2
mabuchi <mabuchi@premiumgraphics.co.jp>
2022-11-16 15:23:20 +0900

    fix build.

--- Crystal/GLSL/SkyBox.fs
+++
@@ -1,8 +0,0 @@
-#version 150
-in vec3 vTexCoord;
-out vec4 fragColor;
-uniform samplerCube cubeMapTex;
-void main(void) {
-	fragColor = texture(cubeMapTex, vTexCoord);
-	//fragColor.rgb = vec3(1,1,1);
-}
