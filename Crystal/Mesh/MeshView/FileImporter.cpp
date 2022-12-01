da7d84616f610bb1a10553ed7b0b645a15f69077
mabuchi <mabuchi@premiumgraphics.co.jp>
2022-12-01 15:08:01 +0900

    implement file importer.

--- Crystal/Mesh/MeshView/FileImporter.cpp
+++ Crystal/Mesh/MeshView/FileImporter.cpp
@@ -3,6 +3,9 @@
 #include "Crystal/File/File/OBJFileReader.h"
 #include "Crystal/File/File/STLFileReader.h"
 
+#include "../Mesh/PolygonMesh.h"
+#include "../Mesh/PolygonMeshBuilder.h"
+
 using namespace Crystal::Math;
 using namespace Crystal::Graphics;
 using namespace Crystal::Mesh;
@@ -22,42 +25,62 @@ bool FileImporter::import(const std::filesystem::path& path)
 
 bool FileImporter::importOBJ(const std::filesystem::path& path)
 {
+	/*
 	OBJFileReader reader;
 	const auto isOk = reader.read(path);
 	if (!isOk) {
 		return false;
 	}
-	/*
-	this->pointCloud = std::make_unique<PointCloud>();
-	const auto positions = reader.getPCD().data.positions;
+
+	this->mesh = std::make_unique<PolygonMesh>();
+	const auto obj = reader.getOBJ();
+	const auto positions = obj.positions;
+	const auto normals = obj.normals;
+	const auto texCoords = obj.texCoords;
+	const auto groups = obj.groups;
 	for (const auto& p : positions) {
-		const ColorRGBAf c(1, 1, 1, 1);
-		this->pointCloud->add(std::make_unique<Point>(p, c));
+		this->mesh->addPosition(p);
 	}
-	return true;
+	for (const auto& n : normals) {
+		this->mesh->addNormal(n);
+	}
+	for (const auto& t : texCoords) {
+		this->mesh->addTexCoord(t);
+	}
+	for (const auto& g : groups) {
+		const auto faces = g.faces;
+		for (const auto& f : faces) {
+			const auto& vs = f.positionIndices;
+			const auto& ns = f.normalIndices;
+			const auto& ts = f.texCoordIndices;
+			//this->mesh->addVertex()
+		}
+	}
 	*/
 	return false;
 }
 
 bool FileImporter::importSTL(const std::filesystem::path& path)
 {
-	/*
 	STLFileReader reader;
-	const auto isOk = reader.read(path);
+	const auto isOk = reader.readAscii(path);
 	if (!isOk) {
 		return false;
 	}
-	this->pointCloud = std::make_unique<PointCloud>();
-	const auto vs = reader.getPLY().vertices;
-	for (const auto& v : vs) {
-		const auto x = v.getValueAs<float>(0);
-		const auto y = v.getValueAs<float>(1);
-		const auto z = v.getValueAs<float>(2);
-		const Vector3df p(x, y, z);
-		const ColorRGBAf c(1, 1, 1, 1);
-		this->pointCloud->add(std::make_unique<Point>(p, c));
+
+	PolygonMeshBuilder builder;
+	const auto stl = reader.getSTL();
+	assert(stl.faceCount == stl.faces.size());
+	for (const auto& f : stl.faces) {
+		const auto vertices = f.triangle.getVertices();
+		const auto p0 = builder.createPosition(vertices[0]);
+		const auto p1 = builder.createPosition(vertices[1]);
+		const auto p2 = builder.createPosition(vertices[2]);
+		builder.createFace(p0, p1, p2);
+		//const auto n0 = builder.createNormal(f.normal);
+		//const auto t0 = builder.createTexCoord(Vector2df(0, 0));
+		//const auto v0 = builder.createVertex(p0, n0, t0);
 	}
+	this->mesh = builder.build();
 	return true;
-	*/
-	return false;
 }
