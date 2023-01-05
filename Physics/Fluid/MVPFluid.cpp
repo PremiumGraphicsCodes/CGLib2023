6c162e588cb9e29d20492aa20a1635fb2062d8f7
mabuchi <mabuchi@premiumgraphics.co.jp>
2023-01-05 10:05:15 +0900

    use random.

--- Physics/Fluid/MVPFluid.cpp
+++ Physics/Fluid/MVPFluid.cpp
@@ -4,6 +4,7 @@
 
 #include <algorithm>
 #include <iterator>
+#include <random>
 
 using namespace Crystal::Math;
 using namespace Crystal::Physics;
@@ -49,9 +50,28 @@ void MVPFluid::removeDegeneratedVolumes()
 MVPVolumeParticle* MVPFluid::create(const Vector3df& position, const float radius, const float weight)
 {
 	auto vp = new MVPVolumeParticle(radius, position);
-	auto mp = new MVPMassParticle(vp, Vector3dd(0, 0, 0), weight);
-	vp->setRestMass(weight * 1.25f);
+	auto mp = new MVPMassParticle(vp, Vector3dd(0, 0, 0), weight / 3.0);
+	vp->setRestMass(weight);
 	vp->addMassParticle(mp);
 	mp->setParent(vp);
+
+	std::mt19937 mt{ std::random_device{}() };
+	std::uniform_real_distribution<float> dist(-1.0, 1.0);
+
+	{
+		const auto x = dist(mt);
+		const auto y = dist(mt);
+		const auto z = dist(mt);
+		Vector3df v(x, y, z);
+		v = glm::normalize(v);
+		auto mp1 = new MVPMassParticle(vp, v, weight / 3.0);
+		vp->setRestMass(weight * 1.25f);
+		vp->addMassParticle(mp1);
+		mp1->setParent(vp);
+		auto mp2 = new MVPMassParticle(vp, -v, weight / 3.0);
+		vp->setRestMass(weight * 1.25f);
+		vp->addMassParticle(mp2);
+		mp2->setParent(vp);
+	}
 	return vp;
 }
