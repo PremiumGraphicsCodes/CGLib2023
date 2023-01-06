046d235cd93ebb13a9c8c9933c684822860e6e76
mabuchi <mabuchi@premiumgraphics.co.jp>
2023-01-06 10:30:21 +0900

    add DFSPH.

---
+++ Physics/Fluid/DFSPHFluid.cpp
@@ -0,0 +1,20 @@
+#include "DFSPHFluid.h"
+
+#include "DFSPHParticle.h"
+
+using namespace Crystal::Math;
+using namespace Crystal::Physics;
+
+DFSPHFluid::DFSPHFluid()
+{
+
+}
+
+Box3df DFSPHFluid::getBoundingBox() const
+{
+	auto bb = Box3df::createDegeneratedBox();
+	for (const auto& p : particles) {
+		bb.add(p->getPosition());
+	}
+	return bb;
+}
---
+++ Physics/Fluid/DFSPHFluid.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "DFSPHParticle.h"
+#include "CGLib/Util/UnCopyable.h"
+#include "CGLib/Math/Box3d.h"
+
+#include <memory>
+#include <list>
+
+namespace Crystal {
+	namespace Physics {
+
+class DFSPHFluid : private UnCopyable
+{
+public:
+	DFSPHFluid();
+
+	~DFSPHFluid() {}
+
+	void addParticle(std::unique_ptr<DFSPHParticle>&& mp) { particles.push_back(std::move(mp)); }
+
+	const std::list<std::unique_ptr<DFSPHParticle>>& getParticles() const { return particles; }
+
+	Math::Box3df getBoundingBox() const;
+
+	float viscosityCoe;
+	float effectLength;
+	float pressureCoe;
+	float density;
+
+private:
+	std::list<std::unique_ptr<DFSPHParticle>> particles;
+};
+
+	}
+}
